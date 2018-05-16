e543dd8db4ade81c406c55281ee322fc01a46389
Nikolai Povlsen <nikolaipovlsen@yahoo.dk>
2018-05-16 13:31:01 +0200

    File class

---
+++ newTwitter/newTwitter/File.swift
@@ -0,0 +1,12 @@
+//
+//  File.swift
+//  newTwitter
+//
+//  Created by elev on 16/05/2018.
+//  Copyright © 2018 Nikolai Povlsen. All rights reserved.
+//
+
+import Foundation
+class File{
+    
+}
