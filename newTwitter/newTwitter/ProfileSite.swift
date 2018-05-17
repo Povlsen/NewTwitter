cb84b8939982e6980cd1474a55faf06a82043eb1
Mikkel Norre Nielsen <mikk633k@hotmail.com>
2018-05-17 08:56:31 +0200

    ProfileSite

---
+++ newTwitter/newTwitter/ProfileSite.swift
@@ -0,0 +1,15 @@
+//
+//  ProfileSite.swift
+//  newTwitter
+//
+//  Created by Elev on 17/05/2018.
+//  Copyright © 2018 Nikolai Povlsen. All rights reserved.
+//
+
+import Foundation
+
+class ProfileSite {
+    private var user:User
+    private var bannerPicture:File?
+    private var tweets:[Post]
+}
